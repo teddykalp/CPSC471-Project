da23f3ffa31be91381782f756aafe06401eb220e
Stephen Dios <stephendiosdeveloper@gmail.com>
2021-03-31 18:16:12 -0600

    Started schedule stored procedure

--- server.js
+++ server.js
@@ -10,19 +10,19 @@ var app = express();
 
 // Establish the sql connection variables
 var mysqlConnection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'password',
-      database: 'work_it',
-      multipleStatements: true
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'work_it',
+  multipleStatements: true
 });
 
 // Connect to sql
-mysqlConnection.connect((err)=> {
-  if(!err)
-  console.log('Connection Established Successfully');
+mysqlConnection.connect((err) => {
+  if (!err)
+    console.log('Connection Established Successfully');
   else
-  console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
+    console.log('Connection Failed!' + JSON.stringify(err, undefined, 2));
 });
 
 
@@ -35,47 +35,47 @@ app.use(express.static(__dirname + '/client'));
 app.use(bodyparser.json());
 
 // Application will be listening on port 3000
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log("Server listening on Port " + PORT);
 });
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/manager', function(req,res){
-	 res.sendFile(__dirname + '/manager.html');
+app.get('/manager', function (req, res) {
+  res.sendFile(__dirname + '/manager.html');
 })
 
-app.get('/crudEmployee', function(req,res){
-	 res.sendFile(__dirname + '/crud-employee.html');
+app.get('/crudEmployee', function (req, res) {
+  res.sendFile(__dirname + '/crud-employee.html');
 })
 
-app.get('/payHistory', function(req,res){
-	 res.sendFile(__dirname + '/payHistory.html');
+app.get('/payHistory', function (req, res) {
+  res.sendFile(__dirname + '/payHistory.html');
 })
 
-app.get('/scheduleManager', function(req,res){
-	 res.sendFile(__dirname + '/scheduleManager.html');
+app.get('/scheduleManager', function (req, res) {
+  res.sendFile(__dirname + '/scheduleManager.html');
 })
 
-app.get('/addSchedule', function(req,res){
-	 res.sendFile(__dirname + '/addSchedule.html');
+app.get('/addSchedule', function (req, res) {
+  res.sendFile(__dirname + '/addSchedule.html');
 })
 
-app.get('/sendNotification', function(req,res){
-	 res.sendFile(__dirname + '/notification.html');
+app.get('/sendNotification', function (req, res) {
+  res.sendFile(__dirname + '/notification.html');
 })
 
 
 // on login, query the database for an id the user entered
-app.get('/login/id=:id', function(req,res){
+app.get('/login/id=:id', function (req, res) {
   var id = [req.params.id][0];
   //var query = 'SELECT * FROM Employee WHERE EID ='.concat(id);
   var call = `CALL EmployeeLogin(${id})`
   console.log(call);
   mysqlConnection.query(call, true, (err, rows, fields) => {
-    if (!err){
+    if (!err) {
       console.log("Sent the following data to client");
       console.log(rows);
       res.send(rows);
@@ -86,12 +86,12 @@ app.get('/login/id=:id', function(req,res){
 });
 
 // view all employees for a manager or sub manager
-app.get("/getEmployees/id=:id", function(req,res){
+app.get("/getEmployees/id=:id", function (req, res) {
   var id = [req.params.id][0];
   console.log(id);
   var call = `CALL ManangeEmployees(${id})`;
   mysqlConnection.query(call, true, (err, rows, fields) => {
-    if (!err){
+    if (!err) {
       console.log("Sent the following data to client");
       console.log(rows);
       res.send(rows);
@@ -102,7 +102,7 @@ app.get("/getEmployees/id=:id", function(req,res){
 });
 
 // update an employee's information
-app.put("/updateEmployee", function(req,res){
+app.put("/updateEmployee", function (req, res) {
   var data = req.body
   var id = data["ID"];
   var firstname = "\"" + data["First Name"] + "\"";;
@@ -112,7 +112,7 @@ app.put("/updateEmployee", function(req,res){
   var dept = data["Dept"];
   var call = `CALL UpdateEmployee(${id},${firstname},${lastname},${phone},${email},${dept})`;
   mysqlConnection.query(call, true, (err, rows, fields) => {
-    if (!err){
+    if (!err) {
       console.log("Sent the following data to client");
       console.log(rows);
       res.send(rows);
@@ -123,12 +123,12 @@ app.put("/updateEmployee", function(req,res){
 });
 
 // delete an employee from the employee table
-app.delete("/deleteEmployee/id=:id", function(req,res){
+app.delete("/deleteEmployee/id=:id", function (req, res) {
   var id = [req.params.id][0];
 });
 
-app.post("/addEmployee", function(req,res){
-  var data = req.body ;
+app.post("/addEmployee", function (req, res) {
+  var data = req.body;
   var manager_id = data["Manager EID"];
   var eid = data["EID"];
   var firstname = "\"" + data["First Name"] + "\"";;
@@ -148,7 +148,7 @@ app.post("/addEmployee", function(req,res){
   var call = `CALL AddEmployee(${manager_id},${eid},${firstname},${lastname},${email},  ${phone},${salary},${is_manager},${is_sub_manager},${is_worker},${pay},${wage}, ${branch},${dept},${store})`;
   console.log(call);
   mysqlConnection.query(call, true, (err, rows, fields) => {
-    if (!err){
+    if (!err) {
       console.log("Sent the following data to client");
       console.log(rows);
       res.send(rows);
@@ -158,6 +158,15 @@ app.post("/addEmployee", function(req,res){
   });
 
 
+});
+
+app.get('/work_it/schedule', (req, res) => {
+  mysqlConnection.query('SELECT * FROM schedule', (err, rows, fields) => {
+    if (!err)
+      res.send(rows);
+    else
+      console.log(err);
+  })
 });
 
 
